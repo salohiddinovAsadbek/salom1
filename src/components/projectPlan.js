492772020ebd694a062191ba058ee2b27ae7ce80
salohiddinovAsadbek <mrxcapitain@gmail.com>
2025-01-25 14:34:37 +0500

    as

---
+++ src/components/projectPlan.js
@@ -0,0 +1,18 @@
+function ProjectPlan() {
+  return (
+    <div className="projectPlan">
+      <div>
+        <h1>Project Plan</h1>
+        <p>
+          There are many variations of the passages of lorem Ipsum from
+          available, majority.
+        </p>
+        <p>
+          <span></span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectPlan;
